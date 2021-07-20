9773d16fa5e482350880eef5a592dadea3cde577
Ken Nguyen <kennj.consultant@gmail.com>
2021-07-20 08:53:18 -0400

    config source code by npx create react app

---
+++ src/components/App/App.js
@@ -0,0 +1,18 @@
+import Header from '../Header/Header';
+import News from '../News/News';
+import './App.scss';
+
+function App() {
+  return (
+    <div className="app">
+      <Header />
+      <News />
+      <News />
+      <News />
+      <News />
+      <News />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Header/Header.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Header = () => {
+    return (
+        <div className="header">
+            header content
+        </div>
+    );
+};
+
+export default Header;
